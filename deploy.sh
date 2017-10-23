b4beae798d52780c4f926e26038bc9f3e6a90717
Sushil Verma <sushil@vermanotes.com>
2017-10-23 16:05:57 +0530

    changes ssh key

--- deploy.sh
+++ deploy.sh
@@ -1,5 +1,5 @@
 echo "Copying files to api droplet.."
-rsync -r --delete-after --quiet $TRAVIS_BUILD_DIR/target/spring-boot-tdd.jar root@9.30.99.175:~/
+rsync -r --delete-after --quiet $TRAVIS_BUILD_DIR root@9.30.99.175:~/
 echo "Copy done!"
 
 #ssh root@9.109.125.77 "cd ~/deploy_script.sh"
