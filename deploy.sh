7ec73446c64fe94f5c7b1d994c761c878f3de573
Sushil Verma <sushil@vermanotes.com>
2017-10-24 15:03:56 +0530

    Testing on Linux VM

--- deploy.sh
+++ deploy.sh
@@ -1,5 +1,7 @@
-echo "Copying files to api droplet.."
-rsync -r --delete-after --quiet $TRAVIS_BUILD_DIR root@9.30.99.175:~/
-echo "Copy done!"
+echo "***** Copying files to api droplet.."
+
+rsync -r --delete-after --quiet $TRAVIS_BUILD_DIR/target/spring-boot-tdd.jar root@13.126.88.66:~/app/
+
+echo "***** Copy done!"
 
 #ssh root@9.109.125.77 "cd ~/deploy_script.sh"
