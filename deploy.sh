1368be485a39b87c671ec963c1d119cb625a7500
Sushil Verma <sushil@vermanotes.com>
2017-10-24 15:44:49 +0530

    Demo to Chaitanya

--- deploy.sh
+++ deploy.sh
@@ -1,8 +1,7 @@
 echo "***** Copying files to api droplet.."
-
-#rsync -r --delete-after --quiet $TRAVIS_BUILD_DIR/target/spring-boot-tdd.jar root@13.126.88.66:/root/app/
 rsync -r --delete-after --quiet $TRAVIS_BUILD_DIR root@13.126.88.66:/root/app/
-
 echo "***** Copy done!"
-
+echo
+echo "***** Executing deployment script."
 ssh root@13.126.88.66 "cd /root/app; chmod +x sample.sh; sh sample.sh"
+echo "***** Executing deployment script done!"
