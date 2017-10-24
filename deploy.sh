b005518ce627cc98ede6a37667ef1159c4d1d3c6
Sushil Verma <sushil@vermanotes.com>
2017-10-24 15:10:28 +0530

    Changed target path

--- deploy.sh
+++ deploy.sh
@@ -1,6 +1,6 @@
 echo "***** Copying files to api droplet.."
 
-rsync -r --delete-after --quiet $TRAVIS_BUILD_DIR/target/spring-boot-tdd.jar root@13.126.88.66:~/app/
+rsync -r --delete-after --quiet $TRAVIS_BUILD_DIR/target/spring-boot-tdd.jar root@13.126.88.66:/root/app/
 
 echo "***** Copy done!"
 
