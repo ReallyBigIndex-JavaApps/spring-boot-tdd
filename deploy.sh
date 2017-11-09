fc69e9a1a18477560514b8c02c21d92352b5988f
Sushil Verma <sushilve@in.ibm.com>
2017-11-10 04:17:53 +0530

    testing 12

--- deploy.sh
+++ deploy.sh
@@ -1,9 +1,13 @@
 #!/bin/bash
 
 echo "***** Copying files to api droplet.."
-rsync -r --delete-after --quiet $TRAVIS_BUILD_DIR root@$DEPLOY_HOST:/root/app/
+rsync -r --delete-after --quiet $TRAVIS_BUILD_DIR root@$TARGET_SERVER_HOST:/root/app/
 echo "***** Copy done!"
 echo
 echo "***** Executing deployment script."
-ssh root@$DEPLOY_HOST "cd /root/app; chmod +x sample.sh; sh sample.sh"
+ssh root@$TARGET_SERVER_HOST "cd /root/app; chmod +x sample.sh; sh sample.sh"
 echo "***** Executing deployment script done!"
+
+echo "***** Start zipping the chaincode folder!"
+zip -r $TRAVIS_BUILD_DIR.zip $TRAVIS_BUILD_DIR
+echo "***** End zipping the chaincode folder!"
